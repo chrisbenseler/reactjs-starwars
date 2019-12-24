0775ae8d8736d2b41cdf49eb7fda15b598a1f098
Christian Benseler <christian.benseler@bv.com.br>
2019-12-24 10:30:28 -0300

    feat: material design

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    
+  },
+  toolbar: {
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  button: {
+    margin: '0 1rem'
+  }
+}));
 
 function Menu() {
-    return <nav>
-    <ul>
-      <li><a href="/movies">Movies</a></li>
-      <li><a href="/planets">Planets</a></li>
-    </ul>
-  </nav> 
+
+  const classes = useStyles();
+  console.log(classes)
+  
+  return <header className={classes.root}>
+    <Toolbar className={classes.toolbar}>
+      <Button
+        variant="outlined"
+        color="primary"
+        classes={{
+          root: classes.button
+        }}
+      >Movies
+      </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        classes={{
+          root: classes.button
+        }}
+      >Planets
+      </Button>
+  </Toolbar>
+  </header>
 }
 
 export default Menu;
