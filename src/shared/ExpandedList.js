22329edfd5c11e753abc41678fc9db57d743243f
Christian Benseler <christian.benseler@bv.com.br>
2020-01-06 17:10:57 -0300

    feat: arrow navigate

--- src/shared/ExpandedList.js
+++ src/shared/ExpandedList.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%'
+    },
+}));
 
 function ExpandedList(props) {
+
+    const classes = useStyles();
 
     return <ExpansionPanel>
         <ExpansionPanelSummary
@@ -18,11 +30,21 @@ function ExpandedList(props) {
             <Typography>{props.title}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-            <List>
+        
+            <List
+            className={classes.root}
+            >
                 {props.items.map(item => <ListItem
-                    onClick={ () => props.callback != null ? props.callback(item) : {} }
+                    button
+                    onClick={() => props.callback != null ? props.callback(item) : {}}
                     key={item}>
                     <ListItemText primary={item} />
+                    {props.callback && <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="Film">
+                            <ArrowForwardIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                    }
                 </ListItem>)}
             </List>
         </ExpansionPanelDetails>
