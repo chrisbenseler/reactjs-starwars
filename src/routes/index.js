99edcfadece97bfa9462748f742579aa97c1af03
Chris Benseler <cbenseler@gmail.com>
2020-01-31 18:57:09 -0300

    feat: planet detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../components/Home';
 import Planets from '../components/Planets';
+import Planet from '../components/Planet';
 import Film from '../components/Film';
 import People from '../components/People';
 
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/index.html" exact component={Home} />
         <Route path="/home" component={Home} />
+        <Route path="/planets/:id" component={Planet} />
         <Route path="/planets" component={Planets} />
         <Route path="/films/:id" component={Film} />
         <Route path="/people/:id" component={People} />
